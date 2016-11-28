4402b1a08a8e676ab4b992aa39d51ddb7107f336
Diego Adolfo <diegoado@gmail.com>
2016-11-27 22:28:55 -0300

    Initial commit

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 var express      = require('express'),
     path         = require('path'),
     logger       = require('morgan'),
+    monoose      = require('mongoose'),
     bodyParser   = require('body-parser'),
     cookieParser = require('cookie-parser');
 
 var app = express();
 
 var port = process.env.PORT || 8080;
+
+// Connect to MongoDB
+monoose.connect('mongodb://localhost:27017/trip_api', function (error) {
+    if (error) {
+        console.error('Error to connect to MongoDB: ' + error)
+    } else {
+        console.log('Connection with MongoDB established with success!')
+    }
+});
 
 app.use(bodyParser());
 app.use(logger('dev'));
