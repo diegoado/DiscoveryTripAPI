6034751b8545b6df5b8c20d1c47e0759db84aa25
Diego Adolfo <Diego Adolfo>
2017-03-31 12:11:19 -0300

    Fix bugs

--- src/routes/events.js
+++ src/routes/events.js
@@ -65,7 +65,7 @@ router.post('/', multer.single('photo'), passport.authenticate('bearer', { sessi
             localization : localization,
             photo        : photo,
             kind         : req.body.kind,
-            price        : req.body.price,
+            price        : req.body.price ? req.body.price : '0',
             keywords     : req.body.keywords,
             startDate  : req.body.startDate ? new Date(req.body.startDate) : new Date(),
             endDate    : new Date(req.body.endDate)
