ab1cbe0e73cd5dcb9711fa60659131bea3e597e6
Diego Adolfo <diegoado@gmail.com>
2017-04-03 21:22:31 -0300

    Fix bug in delete point router

--- src/routes/points.js
+++ src/routes/points.js
@@ -63,8 +63,7 @@ router.delete('/:id', passport.authenticate('bearer', { session: false }), funct
             error.genericErrorHandler(res, err.status, err.code, err.message);
         } else if (!point) {
             error.genericErrorHandler(res, 404, 'user_error', 'Point not found!');
-        } else if (point.ownerId !== req.user.userId) {
-            log.info(typeof point.ownerId + ' ' + typeof req.user.userId);
+        } else if (point.ownerId.toString() !== req.user.userId) {
             error.genericErrorHandler(res, 404, 'user_error', 'Only the owner of the point can remove it!');
         } else {
             point.remove(function (err) {
